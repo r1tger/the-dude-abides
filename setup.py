57bc0006ce345978af7bbb0da73db61e740e0378
Ritger Teunissen <github@ritger.nl>
2021-04-25 13:17:54 +0200

    Clean up CSS. Add viewport to HTML template for note.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thedudeabides',
-    version='0.5.0',
+    version='0.5.1',
     description='The dude abides',
     url='https://github.com/r1tger/the-dude-abides',
     author='Ritger Teunissen',
