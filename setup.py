2fc94c970f7a95d60cb1ba2bf6bddf13055bd900
Ritger Teunissen <github@ritger.nl>
2021-11-07 12:37:00 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thedudeabides',
-    version='0.5.8',
+    version='0.7.0',
     description='The dude abides',
     url='https://github.com/r1tger/the-dude-abides',
     author='Ritger Teunissen',
